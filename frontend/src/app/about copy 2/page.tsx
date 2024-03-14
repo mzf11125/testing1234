25887afdb75e5509890f57d030f2400dee6b0ac1
mzf11125 <77999453+mzf11125@users.noreply.github.com>
2024-03-14 13:26:25 +0700

    Fix routing2

--- frontend/src/app/about copy 2/page.tsx
+++ frontend/src/app/about copy 2/page.tsx
@@ -1,5 +1,3 @@
-import { pages } from "next/dist/build/templates/app-page"
-
 export default function NoPage(){
     return(
         <h1>This is about pages</h1>
--- frontend/src/app/about copy/page.tsx
+++ frontend/src/app/about copy/page.tsx
@@ -1,5 +1,3 @@
-import { pages } from "next/dist/build/templates/app-page"
-
 export default function YesPage(){
     return(
         <h1>This is about pages</h1>
